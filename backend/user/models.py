fca10b5b5f8a17ae36bfa7c37727af3ec50839a2
minji <minji.sql@gmail.com>
2024-08-13 11:31:12 +0900

    model 정의 완료

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,19 @@
+from datetime import timezone
 from django.db import models
+class User(models.Model):
+    userID = models.AutoField(primary_key=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    # erd 변경건
+    userName = models.CharField(max_length=20)
+    isStudent = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    is_deleted = models.BooleanField(default=False)
 
-# Create your models here.
+    def update_date(self):
+        self.updated_at = timezone.now
+        self.save()
+
+    class Meta:
+        db_table = 'user'
