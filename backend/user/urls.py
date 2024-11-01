801c9acfacded957e843236cbd2ef88aae50c4e1
minji <minji.sql@gmail.com>
2024-11-01 15:17:03 +0900

    feat. 회원가입 로그인 API endpoint 변경

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import signinView, signupView
 
 urlpatterns = [
-    path('users/signup/', signupView, name='회원가입'),
-    path('users/signin/', signinView, name='로그인'),
+    path('users/signup', signupView, name='회원가입'),
+    path('users/signin', signinView, name='로그인'),
 ]
